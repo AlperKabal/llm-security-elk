fd561d91cf6b89e0abe0725e92355746cad4cc82
Alper Kabal <alperkabal14@gmail.com>
2026-08-25 10:29:02 +0300

    Added prompt and response detection to app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
+from detection.detect import detect_prompt, detect_response
 
 
 app = FastAPI()
@@ -23,16 +24,44 @@ class ChatRequest(BaseModel):
     session_id: str
     prompt: str
 
+def severityCheck(detection_result):
+    for rule in detection_result["triggered_rules"]:
+        if rule["severity"] in {"high", "critical"}:
+            return True
+    return False
+
+def askOllama(prompt):
+    response = requests.post("http://localhost:11434/api/generate", json={
+        "model": "mistral",
+        "prompt": prompt,
+        "stream": False
+    })
+    return response.json()["response"]
+
+
 @app.post("/chat")
 def post_message(request: ChatRequest):
     user_id = request.user_id
     session_id = request.session_id
     prompt = request.prompt
 
-    response = requests.post("http://localhost:11434/api/generate", json={
-        "model": "mistral",
-        "prompt": prompt,
-        "stream": False
-    })
+    prompt_detection = detect_prompt(prompt)
 
-    return {"response": response.json()["response"]}
+    if severityCheck(prompt_detection):
+        final_response = "Something went wrong. Please try again."
+        blocked = True
+        response = None
+        response_detection = {"triggered_rules": [], "count": 0}
+    else:
+        response = askOllama(prompt)
+        response_detection = detect_response(response)
+        if severityCheck(response_detection):
+            final_response = "Something went wrong. Please try again."
+            blocked = True
+        else: 
+            final_response = response
+            blocked = False
+    return {"response": final_response}
+    
+
+    
